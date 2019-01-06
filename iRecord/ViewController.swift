597741b4a9202a196ff50acef7d7753d6f6032db
Ibrahim <ibrahim@ibrahims-macbook-pro.local>
2019-01-06 20:30:45 +0500

    added ui button to record

--- iRecord/ViewController.swift
+++ iRecord/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
             try recordSession.setActive(true)
             recordSession.requestRecordPermission { (response) in
                 if response {
+                    self.loadRecordingUI()
                     print("Congrates you do have permission now.")
                 }else{
                     print("You don't have permission")
@@ -32,7 +33,19 @@ class ViewController: UIViewController {
         }
         
     }
-
+    func loadRecordingUI() {
+        print("hi")
+        recordButton = UIButton(type: .system)
+        recordButton = UIButton(frame: CGRect(x: 200, y: 100, width: 128, height: 30))
+        recordButton.setTitleColor(UIColor.white, for: .normal)
+        recordButton.setTitle("Tap to Record", for: .normal)
+        recordButton.backgroundColor = UIColor.brown
+        recordButton.addTarget(self, action: #selector(recordNow), for: .touchUpInside)
+        self.view.addSubview(recordButton)
+    }
+    @objc func recordNow(){
+        print("Recording")
+    }
 
 }
 
