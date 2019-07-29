c1a3a74c7eef36cf8026520b27b3ce98cb864dcd
ibrahimak7 <kkibrahim008@gmail.com>
2019-07-29 13:06:20 +0500

    committed

--- iRecord/ViewController.swift
+++ iRecord/ViewController.swift
@@ -8,12 +8,29 @@
 
 import UIKit
 import AVFoundation
-class ViewController: UIViewController {
-    var recordButton: UIButton!
+class ViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    @IBOutlet weak var recordButton: UIButton!
     var recordSession: AVAudioSession!
     var audioRecorder: AVAudioRecorder!
+    var audioTimer: Timer!
+    var paused: Bool = false
+   
+    @IBOutlet weak var timeLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        let fileManager = FileManager.default
+//        let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+//        do {
+//            let fileURLs = try fileManager.contentsOfDirectory(at: documentsURL, includingPropertiesForKeys: nil)
+//            print(fileURLs)
+//            // process files
+//        } catch {
+//            print("Error while enumerating files \(documentsURL.path): \(error.localizedDescription)")
+//        }
+        
+        recordButton.isHidden = true
         // Do any additional setup after loading the view, typically from a nib.
         recordSession = AVAudioSession.sharedInstance()
         
@@ -22,7 +39,7 @@ class ViewController: UIViewController {
             try recordSession.setActive(true)
             recordSession.requestRecordPermission { (response) in
                 if response {
-                    self.loadRecordingUI()
+                    self.recordButton.isHidden = false
                     print("Congrates you do have permission now.")
                 }else{
                     print("You don't have permission")
@@ -33,19 +50,85 @@ class ViewController: UIViewController {
         }
         
     }
-    func loadRecordingUI() {
-        print("hi")
-        recordButton = UIButton(type: .system)
-        recordButton = UIButton(frame: CGRect(x: 200, y: 100, width: 128, height: 30))
-        recordButton.setTitleColor(UIColor.white, for: .normal)
-        recordButton.setTitle("Tap to Record", for: .normal)
-        recordButton.backgroundColor = UIColor.brown
-        recordButton.addTarget(self, action: #selector(recordNow), for: .touchUpInside)
-        self.view.addSubview(recordButton)
+    func getRandom()->Int{
+        return Int(arc4random_uniform(42))+Int(arc4random_uniform(50))
     }
-    @objc func recordNow(){
-        print("Recording")
-    }
+    func startRecording(){
+        let name = "New Recording \(getRandom()).m4a"
+        let audioName = getDocumentDirectory().appendingPathComponent(name)
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioName, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+            
+            audioTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(updateAudioMeter(timer:)), userInfo: nil, repeats: true)
+            changeBtnImage(action: "pause")
 
+        }catch{
+            finishRecording(success: false)
+        }
+    
+    }
+//    @objc func runTimeCode(){
+//        timeLabel.text = "\(Int(audioRecorder.currentTime))"
+//    }
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+        
+        if success {
+            changeBtnImage(action: "audioBtn")
+            audioTimer.invalidate()
+        } else {
+            print("Failed")
+            // recording failed :(
+        }
+    }
+    func getDocumentDirectory()->URL {
+     let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return path[0]
+    }
+    
+    @IBAction func recordBtnTapped(_ sender: Any) {
+        if audioRecorder == nil {
+            startRecording()
+        } else {
+            finishRecording(success: true)
+        }
+    }
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if !flag {
+            finishRecording(success: false)
+        }
+    }
+//    func startTimer(){
+//        audioTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(runTimeCode), userInfo: nil, repeats: true)
+//    }
+    @IBAction func finishRecordButton(_ sender: Any) {
+        finishRecording(success: true)
+    }
+    func changeBtnImage(action: String){
+        recordButton.setImage(UIImage(named: action), for: .normal)
+    }
+    @objc func updateAudioMeter(timer: Timer)
+    {
+        if audioRecorder.isRecording
+        {
+            let hr = Int((audioRecorder.currentTime / 60) / 60)
+            let min = Int(audioRecorder.currentTime / 60)
+            let sec = Int(audioRecorder.currentTime.truncatingRemainder(dividingBy: 60))
+            let totalTimeString = String(format: "%02d:%02d:%02d", hr, min, sec)
+            timeLabel.text = totalTimeString
+            audioRecorder.updateMeters()
+        }
+    }
 }
 
